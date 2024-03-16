241bf56aa546b3483feb3c2e8b3dd17a06a4be3d
Artem Pinaev <artuchebapa@gmail.com>
2024-03-16 13:30:16 +0300

    init

---
+++ app/src/main/java/com/example/pass_manager/App.kt
@@ -0,0 +1,7 @@
+package com.example.pass_manager
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
---
+++ app/src/main/java/com/example/pass_manager/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pass_manager.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pass_manager.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
